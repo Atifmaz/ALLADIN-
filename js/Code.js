983d2f41291d06f5913ec5e5aeea84b360c5a054
Atif Ahmad MAzhari <atifmaz23@gmail.com>
2021-05-20 21:02:31 +0530

    first commit

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("I N F C U T N O",700,150)
+fill("lightblue")
+text("Hint: Excute A Task !!",700,175)
+
+fill("white")
+textSize(15)
+text("E D T S A A S B",100,300)
+fill("lightblue")
+text("Hint: Used For Storing Information !!",100,325)
+}
